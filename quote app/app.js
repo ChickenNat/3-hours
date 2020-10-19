3b11c2ab4363ba136d200afea515e911c3fa059c
ChickenNat <gheadarling@gmail.com>
2020-10-19 21:41:34 +0700

    modif quote app all

---
+++ quote app/app.js
@@ -0,0 +1,29 @@
+const quotes = [{
+    name:'Saitama Sensei',
+    quote:'Just a guy who is Hero for fun.' 
+},{
+    name:'Genos',
+    quote:'Notice me sensei.'
+},{
+    name:'Saitama Sensei',
+    quote:'You gotta train like hell until the point when yoor hair falls out. Thats the only way to become truly strong.'
+},{
+    name:'Garou',
+    quote:'the scarier the better. Dont you think scary is cool?'
+},{
+    name:'t-shirt',
+    quote:'Oppai :3'
+}]
+
+ const quoteBtn = document.querySelector('#quoteBtn');
+ const quoteAuthor = document.querySelector('#quoteAuthor');
+ const quote = document.querySelector('#quote');
+
+ quoteBtn.addEventListener('click', displayQuote);
+
+ function displayQuote (){
+
+    let number = Math.floor(Math.random()*quotes.length);
+    console.log(number);
+ 
+}
